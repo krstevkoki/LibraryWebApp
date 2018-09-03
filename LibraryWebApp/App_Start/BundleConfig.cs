bdd020783b6e45758029164d65234d907b0db2e9
mihailz <mihail.zafirovski@students.finki.ukim.mk>
2018-09-03 16:19:53 +0200

    Installed packages for DataTables and added bundles in BundleConfig.cs

--- LibraryWebApp/App_Start/BundleConfig.cs
+++ LibraryWebApp/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace LibraryWebApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/DataTables/jquery.dataTables.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
@@ -20,11 +21,13 @@ namespace LibraryWebApp
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/DataTables/dataTables.bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                "~/Content/site.css",
+                "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
