cbf22eafd5b6fa2bb4f032c89adce2770b0cc02b
mihailz <mihail.zafirovski@students.finki.ukim.mk>
2018-09-03 21:04:09 +0200

    Review DbSet

--- LibraryWebApp/Models/IdentityModels.cs
+++ LibraryWebApp/Models/IdentityModels.cs
@@ -24,6 +24,7 @@ namespace LibraryWebApp.Models
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
