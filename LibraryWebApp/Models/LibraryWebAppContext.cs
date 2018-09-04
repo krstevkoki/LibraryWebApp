2748eb3baa9e094c2857e76e83c9f57443be5ffd
mihailz <mihail.zafirovski@students.finki.ukim.mk>
2018-09-04 10:54:31 +0200

    Created basic controlers

---
+++ LibraryWebApp/Models/LibraryWebAppContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LibraryWebApp.Models
+{
+    public class LibraryWebAppContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public LibraryWebAppContext() : base("name=LibraryWebAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<LibraryWebApp.Models.Genre> Genres { get; set; }
+
+        public System.Data.Entity.DbSet<LibraryWebApp.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<LibraryWebApp.Models.Book> Books { get; set; }
+
+        public System.Data.Entity.DbSet<LibraryWebApp.Models.Publisher> Publishers { get; set; }
+
+        public System.Data.Entity.DbSet<LibraryWebApp.Models.Review> Reviews { get; set; }
+    }
+}
