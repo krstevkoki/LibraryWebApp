ed5c886ee80eb195fc58d62b81b0ba38d1cc83b7
mihailz <mihail.zafirovski@students.finki.ukim.mk>
2018-09-04 17:50:44 +0200

    Created AddToAuthorModel.cs Class

---
+++ LibraryWebApp/Models/AddToAuthorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryWebApp.Models
+{
+    public class AddToAuthorModel
+    {
+        [Required]
+        [Key]
+        public int BookID { get; set; }
+
+        [Required]
+        [Key]
+        public int AuthorID { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+
+        public AddToAuthorModel()
+        {
+            Books = new List<Book>();
+        }
+    }
+}
