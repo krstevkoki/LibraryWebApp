c07c883d6573ee69c7b6283ad8f4fdc365267db4
mihailz <mihail.zafirovski@students.finki.ukim.mk>
2018-09-07 20:24:18 +0200

    Created User Pannel

--- LibraryWebApp/Controllers/ManageController.cs
+++ LibraryWebApp/Controllers/ManageController.cs
@@ -64,11 +64,16 @@ namespace LibraryWebApp.Controllers
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
+                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
+                Points = 0,
+                DateExpiring = DateTime.Now
             };
             if (User.IsInRole(Roles.Admin))
                 return View("IndexAdmin", model);
+            else if (User.IsInRole(Roles.User))
+                return View("IndexUser", model);
             return View(model);
+
         }
 
         //
--- LibraryWebApp/Models/ManageViewModels.cs
+++ LibraryWebApp/Models/ManageViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
@@ -12,6 +13,8 @@ namespace LibraryWebApp.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+        public int Points { get; set; }
+        public DateTime DateExpiring { get; set; }
     }
 
     public class ManageLoginsViewModel
