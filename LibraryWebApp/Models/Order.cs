5763572e71cc3d42fa7da9fe5548ae2692cf6f69
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-09-12 16:08:03 +0200

    Modify the DataAnnotations for the properties

--- LibraryWebApp/Models/Order.cs
+++ LibraryWebApp/Models/Order.cs
@@ -8,44 +8,41 @@ namespace LibraryWebApp.Models
 {
     public class Order
     {
-        [Required]
-        public int OrderId { get; set; }
+        [ScaffoldColumn(false)] public int OrderId { get; set; }
+
+        [ScaffoldColumn(false)] public string Username { get; set; }
 
         [Required]
-        public string Username { get; set; }
-
-        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
-        public string Address { get; set; }
+        [Required] public string Address { get; set; }
+
+        [Required] public string City { get; set; }
+
+        [Required] public string State { get; set; }
 
         [Required]
-        public string City { get; set; }
-
-        [Required]
-        public string State { get; set; }
-
-        [Required]
+        [Display(Name = "Postal code")]
         public string PostalCode { get; set; }
 
-        [Required]
-        public string Country { get; set; }
+        [Required] public string Country { get; set; }
+
+        [Required] [Phone] public string Phone { get; set; }
 
         [Required]
-        [Phone]
-        public string Phone { get; set; }
-
-        [Required]
+        [Display(Name = "Email Address")]
         [EmailAddress]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        public decimal Total { get; set; }
+        [ScaffoldColumn(false)] public decimal Total { get; set; }
 
-        public DateTime OrderDate { get; set; }
+        [ScaffoldColumn(false)] public DateTime OrderDate { get; set; }
 
         public ICollection<OrderDetail> OrderDetails { get; set; }
 
