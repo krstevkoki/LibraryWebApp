64cdd8c2d8f7f7a3b00253d909119752be686054
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-09-13 15:40:36 +0200

    Add FirstName and LastName to the RegisterViewModel

--- LibraryWebApp/Models/AccountViewModels.cs
+++ LibraryWebApp/Models/AccountViewModels.cs
@@ -77,6 +77,14 @@ namespace LibraryWebApp.Models
         public string ConfirmPassword { get; set; }
 
         [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
         [Display(Name = "Age")]
         [Range(14, 99)]
         public int Age { get; set; }
