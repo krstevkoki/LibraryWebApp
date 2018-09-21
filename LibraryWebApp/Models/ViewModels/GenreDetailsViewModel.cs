3ed52026f77f3f018fe8f08efcaaf5debfb85f33
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-09-21 11:28:47 +0200

    Initial commit

---
+++ LibraryWebApp/Models/ViewModels/GenreDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryWebApp.Models.ViewModels
+{
+    public class GenreDetailsViewModel
+    {
+        public Genre Genre { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
